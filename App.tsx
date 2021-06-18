3c16de58b5129ceddfa59ea252fa294c3109fa4f
unknown <antonio.salazar.emrsn@gmail.com>
2021-06-17 19:12:13 -0500

    initial structure using typescript, installed some npm packages recommended in https://reactnavigation.org/

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {View, Text} from 'react-native';
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Hey there!! Doing well? Still working?</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;
